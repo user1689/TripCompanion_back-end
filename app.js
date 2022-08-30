0b06ae0257badfcfb192d0b667e8ae80089b83ba
sb <89725525+user1689@users.noreply.github.com>
2022-08-29 20:36:10 -0700

    update upload image module

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Acccess-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, OPTIONS, PUT, DELETE");
   next();
 })
 
@@ -44,5 +43,5 @@ mongoose
     app.listen(port, () => console.log(`Example app listening on port ${port}!`));
   })
   .catch(error => {
-    console.log(err);
+    console.log(error);
   })
